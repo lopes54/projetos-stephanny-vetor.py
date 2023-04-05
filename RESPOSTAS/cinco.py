748f3fbc55a71a264aebd98d81dd6d9bfa19664a
Stephanny <doispires06@gmail.com>
2023-04-05 10:47:21 -0300

    first commit

---
+++ RESPOSTAS/cinco.py
@@ -0,0 +1,3 @@
+string = input("Digite uma string: ")  # pede para o usuário digitar uma string
+string_invertida = string[::-1]  # inverte a string usando slicing
+print("A string invertida é:", string_invertida)  # exibe a string invertida
